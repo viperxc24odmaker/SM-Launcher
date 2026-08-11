703b66f7f29fc70dd7cc1c338498d5812abdc2e6
viperxc24odmaker <viperxc2204@gmail.com>
2026-08-11 14:56:18 +0100

    feat: expose mod manager APIs to renderer

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,20 +1,8 @@
 import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('smLauncher', {
-  instances: {
-    list: () => ipcRenderer.invoke('instances:list'),
-    create: (input: unknown) => ipcRenderer.invoke('instances:create', input),
-    delete: (id: string) => ipcRenderer.invoke('instances:delete', id),
-    launch: (id: string) => ipcRenderer.invoke('instances:launch', id)
-  },
-  accounts: {
-    list: () => ipcRenderer.invoke('accounts:list'),
-    offline: (username: string) => ipcRenderer.invoke('accounts:offline', username),
-    elyby: (input: unknown) => ipcRenderer.invoke('accounts:elyby', input),
-    microsoft: () => ipcRenderer.invoke('accounts:microsoft'),
-    remove: (id: string) => ipcRenderer.invoke('accounts:remove', id),
-    setActive: (id: string) => ipcRenderer.invoke('accounts:set-active', id)
-  },
-  onMicrosoftCode: (callback: (event: unknown, code: unknown) => void) => ipcRenderer.on('auth:microsoft-code', callback),
-  openExternal: (url: string) => ipcRenderer.invoke('app:open-external', url)
+contextBridge.exposeInMainWorld('smLauncher',{
+ instances:{list:()=>ipcRenderer.invoke('instances:list'),create:(input:unknown)=>ipcRenderer.invoke('instances:create',input),delete:(id:string)=>ipcRenderer.invoke('instances:delete',id),launch:(id:string)=>ipcRenderer.invoke('instances:launch',id)},
+ accounts:{list:()=>ipcRenderer.invoke('accounts:list'),offline:(username:string)=>ipcRenderer.invoke('accounts:offline',username),elyby:(input:unknown)=>ipcRenderer.invoke('accounts:elyby',input),microsoft:()=>ipcRenderer.invoke('accounts:microsoft'),remove:(id:string)=>ipcRenderer.invoke('accounts:remove',id),setActive:(id:string)=>ipcRenderer.invoke('accounts:set-active',id)},
+ mods:{search:(query:string,loader?:string,version?:string)=>ipcRenderer.invoke('mods:search',query,loader,version),install:(instanceId:string,versionId:string)=>ipcRenderer.invoke('mods:install',instanceId,versionId),list:(instanceId:string)=>ipcRenderer.invoke('mods:list',instanceId)},
+ onMicrosoftCode:(callback:(event:unknown,code:unknown)=>void)=>ipcRenderer.on('auth:microsoft-code',callback),
+ openExternal:(url:string)=>ipcRenderer.invoke('app:open-external',url)
 });
